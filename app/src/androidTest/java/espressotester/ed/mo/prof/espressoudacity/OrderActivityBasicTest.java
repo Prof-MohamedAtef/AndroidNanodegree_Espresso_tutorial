be97871b0256d59b62a10be12d3e7fb2e5f30d24
Prof-MohamedAtef <proofesser@gmail.com>
2018-11-04 15:24:35 -0800

    TESP.01-Exercise-FinishOrderActivityBasicTest

--- app/src/androidTest/java/espressotester/ed/mo/prof/espressoudacity/OrderActivityBasicTest.java
+++ app/src/androidTest/java/espressotester/ed/mo/prof/espressoudacity/OrderActivityBasicTest.java
@@ -1,33 +1,36 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package espressotester.ed.mo.prof.espressoudacity;
 
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
 /**
- * Created by Prof-Mohamed Atef on 11/4/2018.
+ * This test demos a user clicking the decrement button and verifying that it properly decrease
+ * the quantity the total cost.
  */
-@RunWith(AndroidJUnit4.class)
+
+// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
 public class OrderActivityBasicTest {
 
-    @Rule
-    public ActivityTestRule<OrderActivity> mActivityTestRule
-            = new ActivityTestRule<>(OrderActivity.class);
+    // TODO (2) Add the rule that provides functional testing of a single activity
 
-    @Test
-    public void clickIncrementButton_ChangesQuantityAndCost() {
-        // 1. Find the view
-        // 2. perform Action on the View
-        // 3. check if the view does what you expect
-        onView((ViewMatchers.withId(R.id.increment_button))).perform(click());
-        onView(ViewMatchers.withId(R.id.quantity_text_view)).check(matches(withText("1")));
-        onView(ViewMatchers.withId(R.id.cost_text_view)).check(matches(withText("$5.00")));
+    // TODO (3) Finish writing this test which will:
+    //          - Check that the initial quantity is zero
+    //          - Click on the decrement button
+    //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+
+    public void clickDecrementButton_ChangesQuantityAndCost() {
     }
 }
